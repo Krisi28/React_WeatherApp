dcc87511e120445ef0bd9a0e941857a8e4eb5dc8
Krisi28 <72917514+Krisi28@users.noreply.github.com>
2021-02-14 17:45:20 +0100

    added axios

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Weather from './WeatherApp';
 import reportWebVitals from './reportWebVitals';
+import SearchEngine from "./SearchEngine.js";
+import Heading from "./heading.js";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Heading />
+    <SearchEngine />
+    <Weather city="Vienna" />
   </React.StrictMode>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
