52a43c877fa2c1a7872274bd60a96f6a585e6356
Krisi28 <72917514+Krisi28@users.noreply.github.com>
2021-02-14 19:03:28 +0100

    search form css

--- src/footer.js
+++ src/footer.js
@@ -8,5 +8,7 @@ export default function Footer() {
             Coded by Kristina Rericha 
           <br />
           <a href="https://github.com/Krisi28/React_WeatherApp">GitHub</a>
+          <br />
+          <br />
           </div>
 }
