54ea31897d9bfc13b03a1900ab493d7afc6dea34
Krisi28 <72917514+Krisi28@users.noreply.github.com>
2021-02-14 18:59:57 +0100

    css

--- src/footer.js
+++ src/footer.js
@@ -3,6 +3,8 @@ import "./Footer.css";
 
 export default function Footer() {
   return <div> 
+        <br />
+        <br />
             Coded by Kristina Rericha 
           <br />
           <a href="https://github.com/Krisi28/React_WeatherApp">GitHub</a>
