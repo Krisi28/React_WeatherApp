ed077511a90ab31eb5487e800d296c25de2d7803
Krisi28 <72917514+Krisi28@users.noreply.github.com>
2021-02-14 17:56:55 +0100

    added footer

--- src/index.js
+++ src/index.js
@@ -5,12 +5,14 @@ import Weather from './WeatherApp';
 import reportWebVitals from './reportWebVitals';
 import SearchEngine from "./SearchEngine.js";
 import Heading from "./heading.js";
+import Footer from "./footer.js";
 
 ReactDOM.render(
   <React.StrictMode>
     <Heading />
     <SearchEngine />
     <Weather city="Vienna" />
+    <Footer />
   </React.StrictMode>,
   document.getElementById('root')
 );
