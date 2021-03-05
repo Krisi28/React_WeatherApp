cc450a7311e1959f8b3e84ac0208f09b7e682748
Krisi28 <72917514+Krisi28@users.noreply.github.com>
2021-03-05 11:22:15 +0100

    added city vienna in index.js

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ ReactDOM.render(
   <React.StrictMode>
     <Heading />
     <HeadingCurrentWeather />
-    <SearchEngine />
+    <SearchEngine city="Vienna"/>
     <Footer />
   </React.StrictMode>,
   document.getElementById('root')
